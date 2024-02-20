9987cf012754c6b2d1cb83175d45ef2ef112b7aa
msruan <santosruan2021@gmail.com>
2024-02-19 22:18:11 -0300

    cards so expandem na header mas n se sustentam

---
+++ card_hover.js
@@ -0,0 +1,13 @@
+const cards = document.querySelectorAll('.card');
+
+        cards.forEach(card => {
+            const header = card.querySelector('header');
+
+            header.addEventListener('mouseenter', () => {
+                card.classList.add('hovered');
+            });
+
+            header.addEventListener('mouseleave', () => {
+                card.classList.remove('hovered');
+            });
+        });
